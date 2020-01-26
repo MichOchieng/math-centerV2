82122960120eaaefa1b8990ae7277838e7887a90
MichOchieng <MichOchieng@gmail.com>
2020-01-25 22:07:26 -0800

    Moved text input down into a new row

--- math-center/src/App.js
+++ math-center/src/App.js
@@ -8,6 +8,7 @@ import ChatRoomNav from './components/chatRoomNav'
 
 import Jumbotron from 'react-bootstrap/Jumbotron';
 import { Container, Row, Col } from 'react-bootstrap';
+import {Button, InputGroup, FormControl } from 'react-bootstrap';
 
 
 function App() {
@@ -18,6 +19,20 @@ function App() {
         <Col>1 of 3</Col>
         <Col><ChatArea /></Col>
         <Col> <InfoBar /></Col>
+      </Row>
+      <Row>
+        <Col>
+        <InputGroup className="mb-3" id="send-container">
+                    <FormControl
+                    placeholder="Your message"
+                    aria-label="Your message"
+                    aria-describedby="basic-addon2"
+                    />
+                    <InputGroup.Append>
+                    <Button variant="outline-secondary">Send</Button>
+                    </InputGroup.Append>
+          </InputGroup>
+        </Col>        
       </Row>
       
     </div>
