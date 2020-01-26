9cc574dd70738ac2dbcf35a0faec3a1edf88f3ae
Callum <callum.cassidynolan@mail.utoronto.ca>
2020-01-25 20:21:28 -0500

    added rows and stuff

--- math-center/src/App.js
+++ math-center/src/App.js
@@ -1,10 +1,10 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 
 import Jumbotron from 'react-bootstrap/Jumbotron';
+import { Container, Row, Col } from 'react-bootstrap';
 
 
 function App() {
@@ -13,6 +13,18 @@ function App() {
       <Jumbotron>
         <h1 className="header">Welcome To React-Bootstrap</h1>
       </Jumbotron>
+      <Container>
+        <Row>
+          <Col>1 of 3</Col>
+          <Col xs={6}>2 of 3 (wider)</Col>
+          <Col>3 of 3</Col>
+        </Row>
+        <Row>
+          <Col>1 of 3</Col>
+          <Col xs={5}>2 of 3 (wider)</Col>
+          <Col>3 of 3</Col>
+        </Row>
+      </Container>
     </div>
   );
 }
