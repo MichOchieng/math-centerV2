5ae76c13d4122440b1b93f3594e5ef8861324d4e
MichOchieng <MichOchieng@gmail.com>
2020-02-01 02:47:38 -0800

    Added temporary dashboard with ul instead of sticky sidebar

--- math-center/src/components/chatRoomNav.js
+++ math-center/src/components/chatRoomNav.js
@@ -1,13 +1,28 @@
 import React, { Component } from 'react';
+import { Container, ListGroup, Button, InputGroup, FormControl } from 'react-bootstrap';
 
 class ChatRoomNav extends Component {
     render() {
         return (
-            <div>
-               stuff
-            </div>
+            <Container>
+                <div class="chatNav">
+                    <h1 class="chatNavHeader">
+                        Dashboard
+                    </h1>
+                    <ul>
+                        <li>Home</li>
+                        <li>General</li>
+                        <li>Linear Algebra</li>
+                        <li>Discrete Math</li>
+                        <li>Big Brain Chat</li>
+                        <li>General</li>
+
+                    </ul>
+                </div>
+            </Container>
         );
     }
+    
 }
 
 export default ChatRoomNav;
