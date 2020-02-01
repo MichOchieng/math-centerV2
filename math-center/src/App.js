9e5fae027e1eb51e13c9e16eba5911a8ba3c8a13
MichOchieng <MichOchieng@gmail.com>
2020-02-01 00:52:53 -0800

    Reverted to last working version

--- math-center/src/App.js
+++ math-center/src/App.js
@@ -1,37 +1,21 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import './App.css';
-
 import ChatArea from './components/ChatArea'
 import InfoBar from './components/infoBar'
 import ChatRoomNav from './components/chatRoomNav'
 
+import Jumbotron from 'react-bootstrap/Jumbotron';
 import { Container, Row, Col } from 'react-bootstrap';
 import {Button, InputGroup, FormControl } from 'react-bootstrap';
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      response: false,
-      endpoint: "http://localhost:3000"
-    }
-  }
-
-  componentDidMount() {
-    const { endpoint } = this.state;
-    const socket = socketIOClient(endpoint);
-    socket.on("connected", data => this.setState({response: data}))
-  }
-}
 
 function App() {
-  const {response} = this.state;
   return (
     <div className="App">
  
       <Row>
-        <Col>{response}</Col>
+        <Col>1 of 3</Col>
         <Col><ChatArea /></Col>
         <Col> <InfoBar /></Col>
       </Row>
@@ -49,9 +33,8 @@ function App() {
           </InputGroup>
         </Col>        
       </Row>
-      
+
     </div>
   );
 }
-
 export default App;
